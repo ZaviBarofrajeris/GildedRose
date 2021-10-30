fe6db483d2c22f25d5cb6056cff1c35a032d5a6a
ZaviBaroFrajeris <kubko0509@gmail.com>
2021-10-30 15:34:01 +0200

    posmurfil som

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -23,7 +23,73 @@ namespace GildedRose
         public void UpdateQuality()
         {
             // TODO ...
+
             // Hint: Iterate through this.items and check Name property to access specific item
+            foreach (var item in items)
+            {
+                if (item.SellIn > 0 && item.Name != "Aged Brie" && item.Name != "Sulfuras, Hand of Ragnaros" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Conjured Mana Cake")
+                {
+                    item.Quality -= 1;
+                }
+                else if (item.SellIn <= 0 && item.Name != "Aged Brie" && item.Name != "Sulfuras, Hand of Ragnaros" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Conjured Mana Cake")
+                {
+                    item.Quality -= 2;
+                }
+                else if (item.Name == "Aged Brie" && item.SellIn > 0)
+                {
+                    item.Quality += 1;
+                }
+                else if (item.Name == "Aged Brie" && item.SellIn <= 0)
+                {
+                    item.Quality += 2;
+                }
+                else if (item.Name == "Conjured Mana Cake" && item.SellIn > 0)
+                {
+                    item.Quality -= 2;
+                }
+                else if (item.Name == "Conjured Mana Cake" && item.SellIn <= 0)
+                {
+                    item.Quality -= 4;
+                }
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn > 10)
+                    {
+                        item.Quality += 1;
+                    }
+                    else if (item.SellIn > 5 && item.SellIn <= 10)
+                    {
+                        item.Quality += 2;
+                    }
+                    else if (item.SellIn > 0 && item.SellIn <= 5)
+                    {
+                        item.Quality += 3;
+                    }
+                    else if (item.SellIn <= 0)
+                    {
+                        item.Quality = 0;
+                    }
+
+                }
+
+
+
+
+                if (item.Quality < 0) //&& //||
+                {
+                    item.Quality = 0;
+                }
+                if (item.Quality > 50 && item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality = 50;
+                }
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn -= 1;
+                }
+
+
+            }
         }
     }
 }
